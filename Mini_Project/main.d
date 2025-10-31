134f669893209265b9399443b422b1ffd514cea9
K.S.GOPALA KRISHNAN <88239702+GOPALAKRISHNAN-KS@users.noreply.github.com>
2025-10-31 12:01:21 +0530

    Delete Mini_Project directory

--- Mini_Project/main.d
+++
@@ -1,4 +0,0 @@
-mini_project/main.o: ..\Core\Src\main.c \
-  ..\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f446xx.h \
-  ..\Drivers\CMSIS\Include\core_cm4.h \
-  ..\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
